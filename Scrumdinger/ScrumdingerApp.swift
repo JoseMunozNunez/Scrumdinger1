e2b271800f538a6a975be3d6675357a296e0da92
JoseMunozNunez <jose.munoz.nunez@gmail.com>
2023-02-08 00:12:06 -0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Jose Muñoz on 08-02-23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
